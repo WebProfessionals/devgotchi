c02fb91143cd8e15a3d1d5a0aaece84842ea9ecc
Florian Gächter <florian.gaechter@gmail.com>
2016-10-29 18:37:31 +0200

    UI

--- www/xgotchi/ui.js
+++ www/xgotchi/ui.js
@@ -55,7 +55,7 @@ define([
 
     // Stateveränderung
     var stateChangeHandle = state.registerStateChangeCallback(function(e, d) {
-        console.log(e, d);
+        console.log(e);
     });
     // stateChangeHandle.unRegister();
 
