0ab786a7b4a85b06302286732095d6639fe1d3ad
Florian Gächter <florian.gaechter@gmail.com>
2016-10-29 17:27:58 +0200

    UI

--- www/xgotchi/main.js
+++ www/xgotchi/main.js
@@ -5,6 +5,8 @@ define(function (require) {
   // UI geladen
   var ui = require('./ui');
 
+  ui.animationLoop();
+
 
 
   document.addEventListener('click', function () {
--- www/xgotchi/ui.js
+++ www/xgotchi/ui.js
@@ -1,23 +1,64 @@
-define(['pubsub', './state'], function (pubsub, state) {
+define([
+    'pubsub', './state', './animation'
+], function(pubsub, state, animation) {
+    var frameIndex = 0,
+        tickCount = 0,
+        ticksPerFrame = 16,
+        numberOfFrames = 2,
+        currentState = 'neutral';
 
-  // Events
-  var eventCode = function (event, data) {
-    console.log(event);
-  };
+    var animationLoop = function() {
+        window.requestAnimationFrame(animationLoop);
 
-  var uiEvents = pubsub.subscribe('ui', eventCode);
-  //pubsub.unsubscribe(uiEvents);
+        update();
+        render();
+    };
 
+    var update = function() {
+        tickCount += 1;
 
-  // Stateveränderung
-  var stateChangeHandle = state.registerStateChangeCallback(function (e, d) {
-    console.log(e, d);
-  });
-  // stateChangeHandle.unRegister();
+        if (tickCount > ticksPerFrame) {
+            tickCount = 0;
 
+            if (frameIndex < numberOfFrames - 1) {
+                frameIndex += 1;
+            } else {
+                frameIndex = 0;
+            }
+        }
+    };
 
-  var reset = function () {
+    var render = function(string) {
+        var saveArray = animation[currentState].idle.frames[frameIndex].split(','),
+            pixels = document.querySelectorAll('.pixel');
 
-  };
-  return {reset: reset};
+        for (var i = 0, len = pixels.length; i < len; i++) {
+            pixels[i].setAttribute('data-state', 0);
+        }
+
+        for(var i= 0, len = saveArray.length; i < len; i++) {
+            var pixel = saveArray[i].split('|');
+            document.querySelectorAll('[data-x="' + pixel[0] + '"][data-y="' + pixel[1] + '"]')[0].setAttribute('data-state', pixel[2]);
+        }
+    };
+
+    // Events
+    var eventCode = function(event, data) {
+        console.log(event);
+    };
+
+    var uiEvents = pubsub.subscribe('ui', eventCode);
+    //pubsub.unsubscribe(uiEvents);
+
+    // Stateveränderung
+    var stateChangeHandle = state.registerStateChangeCallback(function(e, d) {
+        console.log(e, d);
+    });
+    // stateChangeHandle.unRegister();
+
+    var reset = function() {};
+    return {
+        reset: reset,
+        animationLoop: animationLoop
+    };
 });
