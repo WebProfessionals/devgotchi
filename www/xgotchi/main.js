6b65db7f6b4bb6b7022a0a58315e32250239d831
veith <veithz@gmail.com>
2016-10-30 11:59:56 +0100

    main doku

--- www/xgotchi/main.js
+++ www/xgotchi/main.js
@@ -1,9 +1,12 @@
 define(function(require) {
+    // stammhirn laden
     var brain = require('./mainbrain');
+
+    // inputs einbinden
     var angst = require('./input/quetschen');
     var uebelkeit = require('./input/schuetteln');
 
-    // UI geladen
+    // UI laden
     var ui = require('./ui');
 
     ui.animationLoop();
